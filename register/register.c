71dc3ec9d994ee3eea90e4d0e016e97c5aaa3291
ykomano <yoshikazu.komano@kii.com>
2015-07-30 19:28:33 +0900

    use uuid for vendor thing id

--- register/register.c
+++ register/register.c
@@ -15,17 +15,33 @@ int main(void) {
     }
 
     // initialize application
-    const char* app_id   = "APP ID";
-    const char* app_key  = "APP KEY";
+    const char* app_id   = "APP_ID";
+    const char* app_key  = "APP_KEY";
     const char* site_url = KII_SITE_JP;
 
     kii_app_t app = kii_init_app(app_id, app_key, site_url);
 
+    // generate uuid for vendor thing id
+    FILE* fp = fopen("/proc/sys/kernel/random/uuid", "r");
+    if (fp == NULL) {
+        goto END;
+    }
+
+    char uuid[100];
+    fgets(uuid, 100, fp);
+    fclose(fp);
+
+    // remove '\n'
+    uuid[strlen(uuid) - 1] = '\0';
+
     // register KiiThing
-    char* vendor_thing_id    = "VENDOR THING ID";
-    char* thing_password     = "password";
+    const char* vendor_thing_id    = uuid;
+    const char* thing_password     = "password";
     kii_thing_t kii_thing    = NULL;
     kii_char_t* access_token = NULL;
+
+    printf("Vendor Thing ID: %s\n", vendor_thing_id);
+    printf("Password: %s\n", thing_password);
 
     error_code = kii_register_thing(app, vendor_thing_id, thing_password,
         NULL, NULL, &kii_thing, &access_token);
@@ -36,8 +52,8 @@ int main(void) {
     // print information of registered KiiThing
     kii_char_t* thingId = kii_thing_serialize(kii_thing);
 
-    printf("serialized_thing: %s\n", thingId);
-    printf("access_token: %s\n", access_token);
+    printf("Thing ID: %s\n", thingId);
+    printf("Access Token: %s\n", access_token);
 
     kii_dispose_kii_char(thingId);
     kii_dispose_kii_char(access_token);
